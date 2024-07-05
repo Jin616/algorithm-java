c3b77054d9918731c8bac96a098beb538ecae799
jingseo <coding0616@gmail.com>
2024-07-06 00:09:22 +0900

    26042 식당 입구 대기 줄
    문제에서 요구하는 조건이 나름 신경 쓰였다.
    q에서 peek으로 꺼낼 수 있는 줄 가장 앞의 학생이 아니라,
    방금 q에 집어넣은 마지막 학생의 번호를 요구하는 것이었다.
    학생이 줄을 설 때, 조건의 갱신 및 확인이 필요하므로 if 문을 중첩해서 써야 했다.
    
    학생의 번호를 q에 넣어 주고 문제에서 주어진 조건, q의 사이즈가 기존보다 커졌을 때와 사이즈는 같지만 끝의 학생의 번호가 더
    적은 경우 갱신이 일어나게 만들어 주었다. 명령어가 1이 아닌 경우 밥을 먹고 사라지는 것이기 때문에 poll()를 한번 호출하는
    것으로 끝냈다.

--- Algorithm/src/queue/queue/boj/QueueMain.java
+++ Algorithm/src/queue/queue/boj/QueueMain.java
@@ -4,20 +4,46 @@ import java.io.IOException;
 
 import queue.queue.boj.silver.S5_11866;
 import queue.queue.boj.silver.S5_2161;
+import queue.queue.boj.silver.S5_26042;
 
 public class QueueMain {
 	
 	public static void main(String[] args) throws IOException {
 		// silver
 		// test11866();
-		test2161();
-//		test26042();
+		// test2161();
+		test26042();
 //		test2164();
 //		test10845();
 //		test1158();
 //		test18258();
 //		test15828(;)
 //		test26043();
+	}
+
+	private static void test26042() throws IOException {
+		S5_26042 problem = new S5_26042();
+		
+		String[] inputs = {
+			"5\r\n"
+			+ "1 2\r\n"
+			+ "1 1\r\n"
+			+ "2\r\n"
+			+ "1 3\r\n"
+			+ "2"
+		};
+		
+		String[] outputs = {
+			"2 1"
+		};
+		
+		System.out.print("Test case size " + inputs.length);
+		for (int i = 0; i < inputs.length; i++) {
+			String[] args = {inputs[i]};
+			
+			System.out.print("\nexcept is " + outputs[i] + "\noutput is : ");
+			problem.main(args);
+		}
 	}
 
 	private static void test2161() throws IOException {
--- Algorithm/src/queue/queue/boj/silver/S5_26042.java
+++ Algorithm/src/queue/queue/boj/silver/S5_26042.java
@@ -1,5 +1,43 @@
 package queue.queue.boj.silver;
 
-public class S5_26042 {
+import java.io.*;
+import java.util.*;
 
+public class S5_26042 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new StringReader(args[0]));
+		// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		Queue<Integer> q = new LinkedList<Integer>();
+		
+		int last = Integer.MAX_VALUE;
+		int maxSize = 0;
+		
+		for (int i = 0; i < n; i++) {
+			String[] str = br.readLine().split(" ");
+			
+			if (str[0].equals("1")) {
+				int student = Integer.parseInt(str[1]);
+				q.add(student);
+				
+				if (q.size() > maxSize || (q.size() == maxSize && student < last)) {
+					maxSize = q.size();
+					last = student;
+				}
+			} else {
+				q.poll();
+			}
+				
+		}
+		
+		bw.write("" + maxSize + " " + last);
+		bw.flush();
+		// br.close();
+		// bw.close();
+	}
+	
 }
