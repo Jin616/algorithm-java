f520207297afd2b1183dda1fb304b14a2b7dbdd8
jingseo <coding0616@gmail.com>
2024-07-05 23:51:41 +0900

    2161 카드1
    큐의 기초적인 문제다. 한 장을 버리고, 한 장은 다시 재투입하고, 이 과정을 반복한다.

--- Algorithm/src/queue/queue/boj/QueueMain.java
+++ Algorithm/src/queue/queue/boj/QueueMain.java
@@ -3,13 +3,14 @@ package queue.queue.boj;
 import java.io.IOException;
 
 import queue.queue.boj.silver.S5_11866;
+import queue.queue.boj.silver.S5_2161;
 
 public class QueueMain {
 	
 	public static void main(String[] args) throws IOException {
 		// silver
-		test11866();
-//		test2161();
+		// test11866();
+		test2161();
 //		test26042();
 //		test2164();
 //		test10845();
@@ -17,6 +18,26 @@ public class QueueMain {
 //		test18258();
 //		test15828(;)
 //		test26043();
+	}
+
+	private static void test2161() throws IOException {
+		S5_2161 problem = new S5_2161();
+		
+		String[] inputs = {
+			"7"
+		};
+		
+		String[] outputs = {
+			"1 3 5 7 4 2 6"
+		};
+		
+		System.out.print("Test case size " + inputs.length);
+		for (int i = 0; i < inputs.length; i++) {
+			String[] args = {inputs[i]};
+			
+			System.out.print("\nexcept is " + outputs[i] + "\noutput is : ");
+			problem.main(args);
+		}
 	}
 
 	private static void test11866() throws IOException {
--- Algorithm/src/queue/queue/boj/silver/S5_2161.java
+++ Algorithm/src/queue/queue/boj/silver/S5_2161.java
@@ -1,5 +1,32 @@
 package queue.queue.boj.silver;
 
-public class S5_2161 {
+import java.io.*;
+import java.util.*;
 
+public class S5_2161 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new StringReader(args[0]));
+		// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		Queue<Integer> q = new LinkedList<Integer>();
+		
+		for (int i = 1; i <= n; i++)
+			q.add(i);
+		
+		while (!q.isEmpty()) {
+			bw.write("" + q.poll() + " ");
+			
+			if (!q.isEmpty()) 
+				q.add(q.poll());
+		}
+		
+		bw.flush();
+		// br.close();
+		// bw.close();
+	}
+	
 }
